2b1c2765ddb99bcfffb3336cf78a99cb821e30a0
viganRoi <vigan.roitiv@gmail.com>
2025-09-29 13:25:56 +0200

    ./

--- src/components/hero/Contact.jsx
+++ src/components/hero/Contact.jsx
@@ -4,14 +4,14 @@ const Contact = () => {
     return (
         <div
             className='relative w-full h-full min-h-[1500px] flex flex-col items-center justify-start gap-2 text-center relative py-48'
-            style={{ backgroundImage: `url(/projektet/assets/images/hero/bckContact.jpg)`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+            style={{ backgroundImage: `url(/projektet/assets/images/hero/bckContact.jpg)`, backgroundSize: 'cover', backgroundPosition: '0, 0' }}
         >
-            <div className='absolute -top-50 base-width h-80 bg-secondary hover:bg-secondary/90 duration-300 flex flex-col items-center justify-center group'>
+            <div className='absolute -top-50 base-width h-80 bg-secondary transition-colors duration-300 hover:bg-gradient-to-t hover:from-secondary/100 hover:to-primary/10 flex flex-col items-center justify-center group'>
                 <div className='base-width uppercase flex justify-between items-center flex-wrap'>
                     <h1 className='text-primary montserrat text-600 text-[38px] text-nowrap'>jeni tË interesuar pËr projektin tonË ?</h1>
                     <button className='bg-primary text-secondary rounded-full w-36 h-36 text-nowrap'>Na kontaktoni</button>
                 </div>
-                <div className='absolute -bottom-10 contact-abs w-23/24  flex items-center justify-center overflow-hidden'>
+                <div className='absolute -bottom-10 contact-abs w-23/24  flex items-center justify-center overflow-hidden z-10'>
                     <img src="/projektet/assets/images/hero/bckContact.png" alt="" />
                 </div>
             </div>
