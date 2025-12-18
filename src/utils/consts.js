70a33906cfca78165a7de087607a1747152a32ca
rigonroitiv <rigon.roitiv@gmail.com>
2025-12-18 09:26:47 +0100

    updated vps url

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,6 +1,6 @@
 export const mainUrl = 'https://zambaku.com/projektet/';
 // export const BASE_URL = 'http://localhost:8080';
-export const BASE_URL = 'https://srv412786.hstgr.cloud:7153'
+export const BASE_URL = 'https://srv1174470.hstgr.cloud:7153'
 export const objectImageUrl = 'images/'
 export const planmetricImageUrl = '/projektet/planimetrit/'
 // export const BASE_URL = 'http://localhost:8845';
